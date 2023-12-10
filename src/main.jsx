0466693feff91ad0b8b483d8d5545f06fdfec838
AhmedBedeir <bederahmed0@gmail.com>
2023-12-10 22:30:20 +0200

    update basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,24 +6,29 @@ import Home from "./pages/Home.jsx";
 import ExerciseDetail from "./pages/ExerciseDetail.jsx";
 import P404 from "./components/P404.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      errorElement: <P404 />,
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "exercise/:id",
+          element: <ExerciseDetail />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    errorElement: <P404 />,
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "exercise/:id",
-        element: <ExerciseDetail />,
-      },
-    ],
-  },
-]);
+    basename: "/fitness-gym/",
+  }
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
