ec081e310248e8165ecc69bc13c0f6028d130591
AhmedBedeir <bederahmed0@gmail.com>
2023-12-12 11:47:03 +0200

    test meta env

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,5 +1,6 @@
 const URL = "https://exercisedb.p.rapidapi.com/exercises";
-const youtubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com"
+const youtubeSearchUrl = "https://youtube-search-and-download.p.rapidapi.com";
+console.log(import.meta.env.VITE_RAPID_KEY);
 const OPTIONS = {
   method: "GET",
   headers: {
@@ -10,7 +11,7 @@ const OPTIONS = {
 const youtubeSearchOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    "X-RapidAPI-Key": "f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85",
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
